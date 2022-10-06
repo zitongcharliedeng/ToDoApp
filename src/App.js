e1d6e94bddcdd41a9e4520fc28cdf335ae9b10cc
charlie <charlie@example.com>
2022-10-06 11:10:13 +0100

    afkjhjkah

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [editMode, setEditMode] = useState(false);
+  const [formInput, setFormInput] = useState("");
+
+  const [placeholderText, setPlaceholderText] = useState(
+      editMode === false ? "Add a task here" : "Edit the task here"
+    );
+
+  const [buttonText, setButtonText] = useState(
+      editMode === false ? "Submit add here" : "Submit edit here"
+    );
+
+  const handleChange = (event) => {
+    setFormInput(event.target.value);
+  };
+
+  const [tasklist, setTasklist] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <form>
+          <input onChange={handleChange} placeholder={placeholderText} />
+
+          <button onClick={setTasklist([...tasklist, formInput])}>
+            {buttonText}
+          </button>
+        </form>
+      </div>
+
+      <div>
+        <h1>{formInput}</h1>
+        <h1>{tasklist}</h1>
+      </div>
     </div>
   );
 }
