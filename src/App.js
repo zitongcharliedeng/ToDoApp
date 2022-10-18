f21caf2cbfd380a1b4dba9d3193f3d6916b0378d
charlie <charlie@example.com>
2022-10-18 19:43:34 +0100

    done todo list

--- src/App.js
+++ src/App.js
@@ -1,41 +1,55 @@
 import React, { useState } from "react";
-import "./App.css";
+import { TaskForm } from "./components/TaskForm";
+import { TaskList } from "./components/TaskList";
+import "./styles/App.css";
+import axios from "axios";
 
 function App() {
-  const [editMode, setEditMode] = useState(false);
   const [formInput, setFormInput] = useState("");
-
-  const [placeholderText, setPlaceholderText] = useState(
-      editMode === false ? "Add a task here" : "Edit the task here"
-    );
-
-  const [buttonText, setButtonText] = useState(
-      editMode === false ? "Submit add here" : "Submit edit here"
-    );
-
-  const handleChange = (event) => {
-    setFormInput(event.target.value);
-  };
-
+  const [taskIdToEdit, setTaskIdToEdit] = useState(undefined);
   const [tasklist, setTasklist] = useState([]);
 
+  console.log(tasklist);
+  
+  axios
+    .post("/api/version1/users", {
+      name: "bob",
+      password: "BOB123",
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+        .catch(function (error) {
+          console.log(error);
+        });
+  
   return (
-    <div>
-      <div>
-        <form>
-          <input onChange={handleChange} placeholder={placeholderText} />
-
-          <button onClick={setTasklist([...tasklist, formInput])}>
-            {buttonText}
-          </button>
-        </form>
+    <div className="app">
+      <div className="taskform">
+        <TaskForm
+          formInput={formInput}
+          setFormInput={setFormInput}
+          taskIdToEdit={taskIdToEdit}
+          setTaskIdToEdit={setTaskIdToEdit}
+          tasklist={tasklist}
+          setTasklist={setTasklist}
+        />
+      </div>
+      <div className="tasklist">
+        <TaskList
+          formInput={formInput}
+          setFormInput={setFormInput}
+          taskIdToEdit={taskIdToEdit}
+          setTaskIdToEdit={setTaskIdToEdit}
+          tasklist={tasklist}
+          setTasklist={setTasklist}
+        />
       </div>
 
-      <div>
-        <h1>{formInput}</h1>
-        <h1>{tasklist}</h1>
-      </div>
+     
     </div>
+
+     
   );
 }
 
